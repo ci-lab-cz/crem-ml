4b44317604c965a277bb8af316695f43ebb0ed01
MariBerry <yurievnamaria@gmail.com>
2023-04-18 15:12:18 +0200

    parallelized molecule processing in replacement (but single mol mutate_mol runs on 1 core)

--- frag_replacement_with_crem.py
+++ frag_replacement_with_crem.py
@@ -7,7 +7,8 @@ import numpy as np
 from rdkit import Chem
 from  sirms.files import LoadFragments
 import pandas  as pd
-
+from multiprocessing import Pool
+from functools import  partial
 np.random.seed(1)
 
 # def read_worst_and_ids(input_worst, input_ids):
@@ -61,29 +62,21 @@ def read_worst_and_ids(input_worst, input_ids):
 
     return pd.merge(d, f_ids, how='inner')
 
+def make_replacements(mol, prot_ids, df_of_fragments, path_to_db,
+                        radius,
 
-def make_replacements(input_sdf, input_worst, input_ids, path_to_db, radius, max_size, ncores, prot_ids=None):
-    new_products = []
-    id_mol = 0
-    compounds = Chem.SDMolSupplier(input_sdf, removeHs=False, sanitize=True)
-    df_of_fragments = read_worst_and_ids(input_worst, input_ids)
-    print(df_of_fragments)
-    for mol in compounds:
-        # print(Chem.MolToMolBlock(mol))
+                        max_size):
+        products = []
         try:
             mol_id = str(mol.GetProp('ID'))
             if prot_ids is not None:
                 protected_ids = list(map(int, mol.GetProp(prot_ids).split(',')))
             else:
                 protected_ids = prot_ids
-            # print(protected_ids)
+
             for row in df_of_fragments.loc[df_of_fragments.Compound ==mol_id ,:].iterrows():
                 bad_mol_name, bad_frag_id = row[1].Compound, row[1].ids  # molid and atom ids
-                print(bad_frag_id , bad_mol_name , row[1].Frag_id)
 
-
-                # print(Chem.MolToMolBlock(mol))
-                # print(protected_ids )
                 out = mutate_mol(
                         mol=mol,
                         db_name=path_to_db,
@@ -100,12 +93,13 @@ def make_replacements(input_sdf, input_worst, input_ids, path_to_db, radius, max
                         protected_ids=protected_ids,
                         symmetry_fixes=False,
                         return_rxn=True,
-                        ncores=ncores,
+                        ncores=1,
                         return_rxn_freq=False,
                         return_mol=True,
                         replace_ids=bad_frag_id
 
                     )
+                out = list(out)
 
                 for new_smile, transformation, molobj in out:
                         # new_mol = Chem.MolFromSmiles(new_smile)
@@ -114,19 +108,31 @@ def make_replacements(input_sdf, input_worst, input_ids, path_to_db, radius, max
                         new_mol.SetProp('transformation', transformation)
                         if prot_ids is not None:
                             new_mol.SetProp('protected_ids',','.join(map(str,get_child_protected_atom_ids(new_mol, protected_ids))))
-                        new_products.append( new_mol)
+                        products.append( new_mol)
 
 
         except:
             pass
+        return products
 
+def make_replacements_mp(input_sdf, input_worst, input_ids, path_to_db, radius, max_size, ncores, prot_ids=None):
+    Chem.SetDefaultPickleProperties(Chem.PropertyPickleOptions.AllProps) # mp for molobjects with props
+    p = Pool(ncores)
+
+    new_products = []
+    compounds = Chem.SDMolSupplier(input_sdf, removeHs=False, sanitize=True)
+    df_of_fragments = read_worst_and_ids(input_worst, input_ids)
+    print(df_of_fragments)
+    for res in p.imap_unordered(partial(make_replacements, prot_ids=prot_ids, df_of_fragments=df_of_fragments, path_to_db=path_to_db,
+                        radius=radius, max_size=max_size), compounds):
+        new_products.extend(res)
     return new_products
 
 
 def main(input_sdf, input_worst, input_ids, path_to_db, radius,max_size,  output_product_file, ncores,prot_ids):
 
     print('Replacing fragments ...')
-    products = make_replacements(input_sdf, input_worst, input_ids, path_to_db, radius,max_size, ncores,prot_ids)
+    products = make_replacements_mp(input_sdf, input_worst, input_ids, path_to_db, radius,max_size, ncores,prot_ids)
     w = Chem.SDWriter(output_product_file)
     for m in products:
         w.write(m)
